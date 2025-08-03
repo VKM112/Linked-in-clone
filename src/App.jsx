fd516e252fd42b3f7cad51fd8a450a33e79acccf
vk mohan <vkmohan112@gmail.com>
2025-08-03 19:37:35 +0530

    Added Login page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Login from './components/login';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
